ab2134fb7a2b2044a0c66be9bf403d6a5c9e8d73
Ahmed Al-Hasanat <ahmed.alhasanat@gmail.com>
2024-12-07 02:46:47 +0300

    The type builtin: executable files

--- src/main/java/commands/handlers/TypeHandler.java
+++ src/main/java/commands/handlers/TypeHandler.java
@@ -3,14 +3,50 @@ package commands.handlers;
 import commands.Command;
 import commands.CommandHandler;
 
-import javax.swing.text.html.Option;
-import java.util.Optional;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
 
 public class TypeHandler implements CommandHandler {
 
     @Override
-    public void handle(String args) {
-        System.out.println(Optional.ofNullable(Command.of(args)).map(Command::getDescription).orElse(args + ": not found"));
+    public void handle(String commandName) {
+        try {
+            Command command = Command.of(commandName);
+            if (!Objects.isNull(command)) {
+                System.out.println(command.getDescription());
+                return;
+            }
+
+            String commandPath = findCommandPath(commandName);
+
+            if (!Objects.isNull(commandPath)) {
+                System.out.println(commandName + " is " + commandPath);
+                return;
+            }
+
+            System.out.println(commandName + ": not found");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String findCommandPath(String commandName) throws IOException {
+        String pathEnv = System.getenv("PATH");
+        if (Objects.isNull(pathEnv)) {
+            return null;
+        }
+
+        String[] directoriesPath = pathEnv.split(":");
+        for (String directoryPath : directoriesPath) {
+            Path path = Path.of(directoryPath, commandName);
+            if (Files.isRegularFile(path)) {
+                return path.toString();
+            }
+        }
+
+        return null;
     }
 
 }
