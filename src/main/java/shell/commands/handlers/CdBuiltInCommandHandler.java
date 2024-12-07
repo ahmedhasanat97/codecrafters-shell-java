0b78e6db0bcb0314e0c6485017d0a1f6dd89e056
Ahmed Al-Hasanat <ahmed.alhasanat@gmail.com>
2024-12-07 13:06:40 +0300

    The cd builtin: Relative paths

--- src/main/java/shell/commands/handlers/CdBuiltInCommandHandler.java
+++ src/main/java/shell/commands/handlers/CdBuiltInCommandHandler.java
@@ -4,17 +4,60 @@ import shell.commands.BuiltInCommandHandler;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
 
 public class CdBuiltInCommandHandler implements BuiltInCommandHandler {
 
     @Override
     public void handle(String arguments) {
-        Path absolutePath = Path.of(arguments);
-        if (!Files.exists(absolutePath) || !Files.isDirectory(absolutePath)) {
+        Path pth = Path.of(getPath(arguments));
+        if (!Files.exists(pth) || !Files.isDirectory(pth)) {
             System.out.println("cd: " + arguments + ": No such file or directory");
             return;
         }
-        System.setProperty("user.dir", absolutePath.toString());
+        System.setProperty("user.dir", pth.toString());
+    }
+
+    private String getPath(String arguments) {
+        String[] argumentStrings = arguments.split("/");
+        if (argumentStrings.length == 0) {
+            return "/";
+        }
+
+        ArrayDeque<String> arrayDeque = new ArrayDeque<>();
+
+        if (argumentStrings[0].equals(".") || argumentStrings[0].equals("..")) {
+            String currentDir = System.getProperty("user.dir");
+            String[] currentDirStrings = currentDir.split("/");
+            for (String dir: currentDirStrings) {
+                arrayDeque.addLast(dir);
+            }
+        }
+
+        for (String pathArgument: argumentStrings) {
+            switch (pathArgument) {
+                case ".":
+                    // nothing to do
+                    break;
+                case "..":
+                    if (!arrayDeque.isEmpty()) {
+                        arrayDeque.removeLast();
+                    }
+                default:
+                    arrayDeque.addLast(pathArgument);
+            }
+        }
+
+        if (arrayDeque.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (!arrayDeque.isEmpty()) {
+            builder.append("/").append(arrayDeque.removeFirst());
+        }
+
+        return builder.toString();
     }
 
 }
